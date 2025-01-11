a058f969a702f39dcfbe05982ac0aca46bf734ba
xMaFey <uhlir2002@seznam.cz>
2025-01-11 20:55:48 +0000

    Creating Hotbar

---
+++ Individual Project/Assets/My Assets/Scripts/UI Scripts/InventoryDisplay.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public abstract class InventoryDisplay : MonoBehaviour
+{
+    [SerializeField] MouseItemData mouseInventoryItem;
+    protected InventorySystem inventorySystem;
+    protected Dictionary<InventorySlot_UI, InventorySlot> slotDictionary;
+
+    public InventorySystem InventorySystem => inventorySystem;
+    public Dictionary<InventorySlot_UI, InventorySlot> SlotDictionary => slotDictionary;
+    
+    protected virtual void Start()
+    {
+
+    }
+
+    public abstract void AssignSlot(InventorySystem invToDisplay);
+
+    protected virtual void UpdateSlot(InventorySlot updatedSlot)
+    {
+        foreach (var slot in SlotDictionary)
+        {
+            //Slot value - the "under the hood" inventory slot
+            if (slot.Value == updatedSlot)
+            {
+                //Slot key - the UI representation of the value
+                slot.Key.UpdateUISlot(updatedSlot);
+            }
+        }
+    }
+
+    public void SlotClicked(InventorySlot_UI clickedSlot)
+    {
+        Debug.Log("Slot clicked");
+    }
+}
